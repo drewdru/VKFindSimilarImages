61d81864d7aa4bf2cea92b12448e342a637ff521
Drew Dru <drew.drux@gmail.com>
2016-07-29 00:23:10 +0700

    Find similar images

--- VKDownloadImage.py
+++ VKDownloadImage.py
@@ -1,12 +1,19 @@
 import json
-def downloadImage(images):
+import urllib
+
+def downloadImage(images, isBig = True):
+    '''
+    Download images from vk.com
+    '''
+    
+    size = -1 if isBig else 0
     imgInfo = {
         'img': [
         ]
     }
     for index, image in enumerate(images):
         try:
-            u = urllib.request.urlopen(image['sizes'][0]['src'])
+            u = urllib.request.urlopen(image['sizes'][size]['src'])
             raw_data = u.read()
             u.close()
             f = open('./img/' + str(index) + '.jpg','wb+')
--- VKFindSimilarImages.py
+++ VKFindSimilarImages.py
@@ -1,23 +1,26 @@
 import vk
 import json
-import urllib
+# import urllib
 from PIL import Image
 
 from VKDownloadImage import downloadImage
+from Similar import getThumbnail, findSimilarImages
 
-
-session = vk.AuthSession(app_id = 'APP_ID', 
-	user_login = 'USER_LOGIN', 
-	user_password = 'USER_PASS', 
-	scope = 'photos, wall, groups, offline')
+session = vk.AuthSession(app_id = 'APP_ID',
+    user_login = 'USER_LOGIN', 
+    user_password = 'USER_PASS', 
+    scope = 'photos, wall, groups, offline')
 
 api = vk.API(session, v='5.53', timeout=10)
 print('api is connected')
 
 photos = api.photos.get(owner_id = 'ALBUM_OWNER', 
-	album_id = 'ALBUM_ID', 
-	photo_sizes='1')
+    album_id = 'ALBUM_ID', 
+    photo_sizes='1')
 
-downloadImage(photos['items'])
+downloadImage(photos['items'], isBig = False)
 
-
+size = 32,32
+imgDir = './img/'
+getThumbnail(imgDir, size)
+findSimilarImages(imgDir, size)

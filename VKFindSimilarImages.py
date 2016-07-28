f638216eeff58e69874fe4ca266b5d6ad2d3dc24
Drew Dru <drew.drux@gmail.com>
2016-07-28 20:13:05 +0700

    refactoring

--- VKFindSimilarImages.py
+++ VKFindSimilarImages.py
@@ -1,6 +1,10 @@
 import vk
 import json
 import urllib
+from PIL import Image
+
+from VKDownloadImage import downloadImage
+
 
 session = vk.AuthSession(app_id = 'APP_ID', 
 	user_login = 'USER_LOGIN', 
@@ -14,22 +18,6 @@ photos = api.photos.get(owner_id = 'ALBUM_OWNER',
 	album_id = 'ALBUM_ID', 
 	photo_sizes='1')
 
-textFile = open('./textFile.txt','w+')
-for index, image in enumerate(photos['items']):
-    try:
-        print(image['sizes'][0]['src'])
-        u = urllib.request.urlopen(image['sizes'][0]['src'])
-        raw_data = u.read()
-        u.close()
-        f = open('./img/' + str(index) + '.jpg','wb+')
-        f.write(raw_data)
-        f.close()
-        text = str(index)+ ': ' + json.dumps(image['sizes'][0]['src'])
-        print(text)
-        textFile.write(text+'\n')
-    except Exception as inst:
-        print(inst)
-        print('error:' + json.dumps(image['sizes'][0]['src']))
-textFile.close()
+downloadImage(photos['items'])
 
 

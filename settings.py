f9885c0e38f7aa42da88064611ce868bca4f7c91
Drew Dru <drew.drux@gmail.com>
2017-09-04 12:13:16 +0700

    refactoring

---
+++ settings.py
@@ -0,0 +1,29 @@
+DATABASES = {
+    'sqlalchemy': {
+        'drivername': 'DRIVERNAME',
+        'host': 'HOST',
+        'port': 'PORT',
+        'username': 'USERNAME',
+        'password': 'PASSWORD',
+        'database': 'DATABASE'
+    },
+}
+
+VK = {
+    'app_id': 'APP_ID',
+    'user_login': 'USER_LOGIN',
+    'user_password': 'USER_PASSWORD',
+    'scope': 'photos, wall, groups, offline',
+    'owner_id': 'OWNER_ID',
+    'isResume': False,
+    'resumeAlbumID': 0,
+    'albumBlackList': [0],
+}
+
+try:
+    from .local_settings import *
+except Exception:
+    try:
+        from local_settings import *
+    except ImportError:
+        pass
